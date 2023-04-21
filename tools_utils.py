6ea25b724a7f7c0d1b11c28ae8ee68790e6d759a
VongolaWu <vongola8757@gmai.com>
2023-04-21 21:43:53 +0800

    rearrange the prompts

--- tools_utils.py
+++ tools_utils.py
@@ -5,3 +5,15 @@ def prompts(name, description):
         return func
 
     return decorator
+
+
+class ToolName:
+    def __init__(self):
+        # write the initialization code here
+        # according to your design
+        pass
+
+    @prompts(name="THE TOOLS MEANINGFUL NAME",
+             description="THE TOOLS MEANINGFUL DESCRIPTION")
+    def forward(self, inputs):
+        pass
